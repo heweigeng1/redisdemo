4a57a2fef9bd4097d88f97e342226cceb4c17712
heweigeng1 <6318275@qq.com>
2017-07-29 16:35:18 +0800

    初始化

---
+++ UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string key = "testabccc";
+            //设置与获取
+            for (int i = 0; i < 100; i++)
+            {
+                var test =TestService.Instance();
+                test.Set(key, $"{Guid.NewGuid()}");
+                test.Get(key);
+            }
+            //推送
+            //test.Pub("testsub", "hello sub");
+            //订阅
+            //test.Sub("testsub");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ core/TestService.cs
@@ -0,0 +1,53 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class TestService
+    {
+        private static ConnectionMultiplexer redis;
+        private static TestService _instance { get; set; }
+        private TestService()
+        {
+            if (redis==null)
+            {
+                redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            }
+        }
+        public static TestService Instance()
+        {
+            if (_instance==null)
+            {
+                _instance = new TestService();
+            }
+            return _instance;
+        }
+        public void Get(string key)
+        {
+            var db = redis.GetDatabase();
+            string guid = db.StringGet(key);
+            Console.WriteLine(guid);
+        }
+        public void Set(string key,string value)
+        {
+            var db = redis.GetDatabase();
+            db.StringSet(key, value);
+        }
+        public void Pub(string subname,string msg)
+        {
+            ISubscriber sub = redis.GetSubscriber();
+            sub.Publish(subname, msg);
+        }
+        public void Sub(string subname)
+        {
+            ISubscriber sub = redis.GetSubscriber();
+            sub.Subscribe(subname, (channel, message) => {
+                Console.WriteLine((string)message);
+            });
+        }
+    }
+}
